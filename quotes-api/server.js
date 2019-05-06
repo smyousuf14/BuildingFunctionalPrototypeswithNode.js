3670f342e6ee3dbdfb82a19b07174846db2cbd1e
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-05-06 15:03:16 -0400

    Add files via upload

--- quotes-api/server.js
+++ quotes-api/server.js
@@ -3,10 +3,45 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(request, response){
+var quotes = [
+{
+	id:1,
+	quote: "The best is yet to come",
+	author: "Unknown",
+	year: 2000
 
-	response.send("Get request received at '/' ");
+},
+{
+    id: 2,
+    quote: "This is a quote",
+    author: "First Last",
+    year: 1930
+},
+{
+    id: 3,
+    quote: "This is another quote",
+    author: "First2 Last2",
+    year: 1910
+}
+];
+
+
+
+
+app.get("/quotes", function(request, response){
+
+	/*response.send("Get a list of all quotes as json");
+	response.json(quotes); */ 
 	
+	if(request.query.year)
+	{
+		response.send("Return a list of quotes from the year " + request.query.year);
+		
+	}
+	else
+	{
+		response.json(quotes);
+	}
 
 });
 
